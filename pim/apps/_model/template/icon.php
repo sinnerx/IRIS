f93e173666583b9779a6a397a0512992303a4c2e
eimihar <newrehmi@gmail.com>
2014-06-02 11:27:03 +0800

    template/icon@status

--- pim/apps/_model/template/icon.php
+++ pim/apps/_model/template/icon.php
@@ -5,13 +5,16 @@ namespace model\template;
 class Icon
 {
 	## 0 or 1 or 2
-	public function status($no = null)
+	public function status($no = null,$title = null)
 	{
-		$arr[0]	= "<span class='fa fa-circle' style='color:grey;'></span>";
-		$arr[1]	= "<span class='fa fa-circle' style='color:green;'></span>";
-		$arr[2] = "<span class='fa fa-circle' style='color:red;'></span>";
+		$title	= $title?"title='$title'":"";
+		$arr[0]	= "<span {title} class='fa fa-circle' style='color:grey;'></span>";
+		$arr[1]	= "<span {title} class='fa fa-circle' style='color:green;'></span>";
+		$arr[2] = "<span {title} class='fa fa-circle' style='color:red;'></span>";
+		$arr[3] = "<span {title} class='fa fa-circle' style='color:yellow;'></span>";
+		$arr[4] = "<span {title} class='fa fa-circle' style='color:blue;'></span>";
 
-		return $no != null?$arr[$no]:$arr;
+		return $no != null?str_replace('{title}',$title,$arr[$no]):$arr;
 	}
 }
 
