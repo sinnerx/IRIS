ce05727f5adf38ecb49af79d00201d2924417cf6
eimihar <newrehmi@gmail.com>
2014-08-15 15:43:26 +0800

    Event Type : Added for requirement

--- pim/apps/_model/activity/event.php
+++ pim/apps/_model/activity/event.php
@@ -6,7 +6,14 @@ class Event
 	public function type($id = null)
 	{
 		$arr	= Array(
-					1=>"Lawatan"
+					1=>"Lawatan",
+					2=>"Hari Terbuka Pi1M",
+					3=>"Perasmian Pi1M",
+					4=>"Kempen & Promosi",
+					5=>"Kaji Selidik",
+					6=>"Pemeriksaan Audit",
+					7=>"Program Bersama Komuniti",
+					99=>"Lain-lain"
 						);
 
 		return $id?$arr[$id]:$arr;
