1a951776f4aad2f09c0bf1701d3734acf1efb6a2
Raqib <raqib@digitalgaia.com>
2014-10-13 17:42:38 +0800

    #174 Frontend /comment/placeholder-text

--- pim/apps/frontend/_view/comment/addComment.php
+++ pim/apps/frontend/_view/comment/addComment.php
@@ -1,10 +1,27 @@
+<style type="text/css">
+	::-webkit-input-placeholder { /* WebKit browsers */
+       color:    #9e9e9e;
+       opacity:  0.65;
+	}
+	:-moz-placeholder { /* Mozilla Firefox 4 to 18 */
+	   color:    #9e9e9e;
+	   opacity:  1;
+	}
+	::-moz-placeholder { /* Mozilla Firefox 19+ */
+	   color:    #9e9e9e;
+	   opacity:  1;
+	}
+	:-ms-input-placeholder { /* Internet Explorer 10+ */
+	   color:    #9e9e9e;
+	}
+</style>
 								<div class="forum-comment-form">
 									<div class="comment-user-avatar"></div>
 									<div class="comment-post-input">
 										<h3><?php echo $c_user['userProfileFullName']; ?> (Logged-in)</h3>
 										<div class="comment-text-input">
 											<div class="comment-text-input-arrow"></div>
-											<textarea id="body">Taipkan komen anda di sini...</textarea>
+											<textarea id="body" placeholder="Taipkan komen anda di sini..."></textarea>
 											<br/><br/>
 											<a href='javascript:comment.add();' class="bttn-submit">Hantar</a>
 										</div>
