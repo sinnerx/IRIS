dded59c292c6664612b9fe606d9b0d988fd6e322
eimihar <newrehmi@gmail.com>
2015-10-12 13:57:01 +0800

    #290 /frontend/activity/month header text

--- pim/apps/frontend/_view/activity/index_month.php
+++ pim/apps/frontend/_view/activity/index_month.php
@@ -53,7 +53,7 @@ echo model::load("template/frontend")
 <div class="block-content clearfix">
 	<div class="page-content">
 		<div class="page-description">
-		Lorem ipsum dolor sit amet, maiores ornare ac fermentum, imperdiet ut vivamus a, nam lectus at nunc. Quam euismod sem, semper ut potenti pellentesque quisque. In eget sapien sed, sit duis vestibulum ultricies, placerat morbi amet vel, nullam in in lorem vel. In molestie elit dui dictum, praesent nascetur pulvinar sed, in dolor pede in aliquam, risus nec error quis pharetra. Eros metus quam augue suspendisse, metus rutrum risus erat in.  In ultrices quo ut lectus, etiam vestibulum urna a est, pretium luctus euismod nisl, pellentesque turpis hac ridiculus massa. Venenatis a taciti dolor platea, curabitur lorem platea urna odio.
+		Pi1M bertujuan memberi pendedahan kepada masyarakat tempatan terhadap teknologi komputer dan internet. Ia juga meningkatkan taraf ekonomi dan sosial masyarakat melalui kemudahan yang disediakan dan aktiviti yang dijalankan di PI1M. Berikut adalah senarai aktiviti yang di jalankan pada bulan ini. Klik pada aktiviti tersebut untuk maklumat lanjut.
 		</div>
 		<div class="page-sub-wrapper calendar-page clearfix">
 			<div class="calendar-left-details">
