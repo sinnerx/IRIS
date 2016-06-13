3105cddf999280a94ee46bbc9edf673f82b8a2ff
eimihar <newrehmi@gmail.com>
2016-06-13 11:44:56 +0800

    #348 typo fix

--- pim/apps/skmm/_template/default.php
+++ pim/apps/skmm/_template/default.php
@@ -117,7 +117,7 @@
                   <?php if(in_array(authData("current_site.stateID"),Array(12,13))):?>
                     8.00 pagi - 5.00 petang
                   <?php else:?>
-                    9.00 page - 6.00 petang
+                    9.00 pagi - 6.00 petang
                   <?php endif;?>
 
                     </div>
