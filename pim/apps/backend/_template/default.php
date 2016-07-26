11399c9bf900b86fe36239f78ddbdcfb0fb0b09e
kerusiperak <ameeradnan@gmail.com>
2016-07-26 20:53:03 +0800

    - clean up avatar remark

--- pim/apps/backend/_template/default.php
+++ pim/apps/backend/_template/default.php
@@ -280,7 +280,6 @@
       <div class="dropdown">
   <a href="#" class="dropdown-toggle" data-toggle="dropdown">
     <span class="thumb avatar pull-left m-r">                        
-<!--      <img src="<?php //echo url::asset("_scale/images/a0.png");?>" class="dker">-->
                 <?php if(!$user['userProfileAvatarPhoto'] ){
                     $user['userProfileAvatarPhoto'] = "../emptyavatar.png";
                 }
