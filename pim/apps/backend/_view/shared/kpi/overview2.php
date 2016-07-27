89a2391f4006e8964c5607487a1dff5884bdc0c4
Asrul Azwan <asrul@fulkrum.net>
2016-07-27 16:14:52 +0800

    Fix table sort

--- pim/apps/backend/_view/shared/kpi/overview2.php
+++ pim/apps/backend/_view/shared/kpi/overview2.php
@@ -124,17 +124,20 @@ var site = new function()
 		</div>
 		
 		<div class="table-responsive">
-			<table class='table ' border='0'>
+			<table class='table' id="ntable" border='0'>
+				<thead>
 				<tr>
 					<th></th> 		
-					<th>Site </th> 				
-					<th>Event </th>	
-					<th>Entrepreneurship Class </th>	
-					<th>Entrepreneurship Program </th>
-					<th>Training (hours) </th>	
+					<th>Site</th> 				
+					<th>Event</th>	
+					<th>Entrepreneurship Class</th>	
+					<th>Entrepreneurship Program</th>
+					<th>Training (hours)</th>	
 					<th>Active member (Login)</th>
-					<th class='number'>Population </th>	
+					<th>Population</th>	
 				</tr>
+				</thead>
+				<tbody>
 				<?php $completionClass = function($total, $type) use($max)
 				{
 					if($total >= $max[$type])
@@ -169,29 +172,37 @@ var site = new function()
 						</td>
 					</tr>
 				<?php endforeach;?>
+				</tbody>
 			</table>
 		</div>
-		<script src="<?php echo url::asset("backend/js/sorting.js"); ?>"></script>
-		<script src="<?php echo url::asset("backend/js/jquery.tablesort.min.js"); ?>"></script>
+		<script src="<?php //echo url::asset("backend/js/sorting.js"); ?>"></script>
+		<script src="<?php //echo url::asset("backend/js/jquery.tablesort.min.js"); ?>"></script>
+		<script src="<?php echo url::asset("backend/js/jquery.tablesorter.min.js"); ?>"></script>
 		<script type="text/javascript">
 
 	$(function() {
 
-		var table = $('<table class="ex-2"></table>');
-		table.append('<thead><tr><th class="number">Number</th></tr></thead>');
-		var tbody = $('<tbody></tbody>');
+		// var table = $('<table class="ex-2"></table>');
+		// table.append('<thead><tr><th class="number">Number</th></tr></thead>');
+		// var tbody = $('<tbody></tbody>');
 
-		for(var i = 0; i<6000; i++) {
-			tbody.append('<tr><td>' + Math.floor(Math.random() * 100) + '</td></tr>');
-		}
-		table.append(tbody);
-		$('.example.ex-2').append(table);
+		// for(var i = 0; i<6000; i++) {
+		// 	tbody.append('<tr><td>' + Math.floor(Math.random() * 100) + '</td></tr>');
+		// }
+		// table.append(tbody);
+		// $('.example.ex-2').append(table);
 
-		$('table').tablesort().data('tablesort');
-
-		$('thead th.number').data('sortBy', function(th, td, sorter) {
-			return parseInt(td.text(), 10);
+		//$('table').tablesort().data('tablesort');
+		
+		$("#ntable").tablesorter({
+			headers : { 
+				0 : { sorter: false }
+			}
 		});
+
+		// $('thead th.number').data('sortBy', function(th, td, sorter) {
+		// 	return parseInt(td.text(), 10);
+		// });
 
 		// Sorting indicator example
 		// $('table.ex-2').on('tablesort:start', function(event, tablesort) {
@@ -199,10 +210,10 @@ var site = new function()
 		// 	$('.ex-2 th.number').removeClass("sorted").text('Sorting..');
 		// });
 
-		$('table.ex-2').on('tablesort:complete', function(event, tablesort) {
-			$('table.ex-2 tbody').removeClass("disabled");
-			$('.ex-2 th.number').text('Number');
-		});
+		// $('table.ex-2').on('tablesort:complete', function(event, tablesort) {
+		// 	$('table.ex-2 tbody').removeClass("disabled");
+		// 	$('.ex-2 th.number').text('Number');
+		// });
 
 
 	});
