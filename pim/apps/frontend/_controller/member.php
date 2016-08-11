bf6971a85e800be15370cff1b025e977fba9a1b3
alannoh <azlan@fulkrum.net>
2016-08-11 17:40:42 +0800

    1) member activity history array combination

--- pim/apps/frontend/_controller/member.php
+++ pim/apps/frontend/_controller/member.php
@@ -40,7 +40,7 @@ class Controller_Member
 		$userActivity	= model::load("user/activity");
 
 
-		$data['activities']				= $userActivity->getActivities(null,$userID,null,null,10);
+		$data['activities']				= $userActivity->getAllActivities(null,$userID,null,null,10);
 		$data['activities_forum'] 		= $userActivity->getActivities(null,$userID,"forum",null,3);
 		$data['activities_comment'] 	= $userActivity->getActivities(null,$userID,"comment",null,3);
 		$data['activities_activity'] 	= $userActivity->getActivities(null,$userID,"activity",null,3);
